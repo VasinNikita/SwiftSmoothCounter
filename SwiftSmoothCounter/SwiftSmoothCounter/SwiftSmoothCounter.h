c699f191184d7f45633d9304c35eadaa48d8010b
VasinNikita <na.vasin@ya.ru>
2020-06-29 05:29:56 +0300

    update project

---
+++ SwiftSmoothCounter/SwiftSmoothCounter/SwiftSmoothCounter.h
@@ -0,0 +1,18 @@
+//
+//  SwiftSmoothCounter.h
+//  SwiftSmoothCounter
+//
+//  Created by Никита Васин on 29.06.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftSmoothCounter.
+FOUNDATION_EXPORT double SwiftSmoothCounterVersionNumber;
+
+//! Project version string for SwiftSmoothCounter.
+FOUNDATION_EXPORT const unsigned char SwiftSmoothCounterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSmoothCounter/PublicHeader.h>
+
+
